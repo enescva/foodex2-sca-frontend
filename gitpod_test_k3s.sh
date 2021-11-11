236c517f2c7a5273545a2ff986d51c9c84a9782a
enescva <87376170+enescva@users.noreply.github.com>
2021-11-11 17:16:03 +0100

    Update gitpod_test_k3s.sh

--- gitpod_test_k3s.sh
+++ gitpod_test_k3s.sh
@@ -1,2 +1,14 @@
+# kernel dev environment
+sudo apt update -y
+sudo apt install qemu qemu-system-x86 linux-image-$(uname -r) libguestfs-tools sshpass netcat -y
+sudo curl -o /usr/bin/kubectl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x /usr/bin/kubectl
+.gitpod/prepare-rootfs.sh
+.gitpod/qemu.sh
+# prepare k3s
+.gitpod/prepare-k3s.sh
+
+# create docker image
 docker build -t foodex2sca:front . 
+# launch service
 kubectl create -f ./manifests/deployment.local.yml
