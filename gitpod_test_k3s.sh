23a0f32e1f7e14e9fb48e7223122b7110ac5c1b5
enescva <87376170+enescva@users.noreply.github.com>
2021-11-11 17:36:37 +0100

    Update gitpod_test_k3s.sh

--- gitpod_test_k3s.sh
+++ gitpod_test_k3s.sh
@@ -3,8 +3,7 @@ sudo apt update -y
 sudo apt install qemu qemu-system-x86 linux-image-$(uname -r) libguestfs-tools sshpass netcat -y
 sudo curl -o /usr/bin/kubectl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x /usr/bin/kubectl
-.gitpod/prepare-rootfs.sh
-.gitpod/qemu.sh
+.gitpod/prepare-rootfs.sh && .gitpod/qemu.sh &
 # prepare k3s
 .gitpod/prepare-k3s.sh
 
